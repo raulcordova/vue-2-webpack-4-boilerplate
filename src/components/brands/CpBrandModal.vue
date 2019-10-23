ca4c403701ddaa88433d06b7497ecf6465545a8f
Renzo Carpio <me@renzocarpio.com>
2019-10-23 13:11:53 -0500

    se agrego validación de los campos en el formulario brands

--- src/components/brands/CpBrandModal.vue
+++ src/components/brands/CpBrandModal.vue
@@ -1,21 +1,30 @@
 <template lang="pug">
-   CpModalBase(modalTitle='Marca',:showDialog='showDialog',:closeDialog='closeDialog', :actionDialog='actionDialog' :getListBrands='getListBrands')
+   CpModalBase(modalTitle='Marca',:showDialog='showDialog',:closeDialog='closeDialogBrand', :actionDialog='actionDialog' :getListBrands='getListBrands')
     .form
-      md-field
+      md-field(:class="getValidationClass('code')")
         label Código
         md-input(v-model='brand.code' autofocus='')
-      md-field
+        span.md-error(v-if='!$v.brand.code.required') requerido
+        span.md-error(v-else-if='!$v.brand.code.minLength') mínimo 3 caracteres
+      md-field(:class="getValidationClass('name')")
         label Nombre
         md-input(v-model='brand.name')
-      md-field
+        span.md-error(v-if='!$v.brand.name.required') requerido
+        span.md-error(v-else-if='!$v.brand.name.minLength') mínimo 3 caracteres        
+      md-field(:class="getValidationClass('icon')")
         label Url del logo
         md-input(v-model='brand.icon')                  
+        span.md-error(v-if='!$v.brand.icon.required') requerido
+        span.md-error(v-else-if='!$v.brand.icon.url') url no válida        
 </template>
 <script>
 import { mapGetters, mapActions } from "vuex";
+import { validationMixin } from "vuelidate";
+import { required, url, minLength } from "vuelidate/lib/validators";
 import CpModalBase from "@/components/base/modulo/CpModalBase";
 export default {
   components: { CpModalBase },
+  mixins: [validationMixin],
   props: {
     showDialog: {},
     closeDialog: {},
@@ -32,20 +41,52 @@ export default {
       }
     }
   },
+  validations: {
+    brand: {
+      code: {
+        required,
+        minLength: minLength(3)
+      },
+      name: {
+        required,
+        minLength: minLength(3)
+      },
+      icon: {
+        required,
+        url
+      }
+    }
+  },
   methods: {
     ...mapActions("brand", ["add"]),
+    getValidationClass(fieldName) {
+      const field = this.$v.brand[fieldName];
+      if (field) {
+        return {
+          "md-invalid": field.$invalid && field.$dirty
+        };
+      }
+    },
     actionDialog() {
-      this.$store.commit("user/changeLoader");
-      this.add(this.brand)
-        .then(res => {
-          this.$store.commit("user/changeLoader");
-          this.closeDialog();
-          this.getListBrands();
-        })
-        .catch(() => {
-          this.$store.commit("user/changeLoader");
-          console.log("Error agregar brand");
-        });
+      this.$v.$touch();
+
+      if (!this.$v.$invalid) {
+        this.$store.commit("user/changeLoader");
+        this.add(this.brand)
+          .then(res => {
+            this.$store.commit("user/changeLoader");
+            this.closeDialog();
+            this.getListBrands();
+          })
+          .catch(() => {
+            this.$store.commit("user/changeLoader");
+            console.log("Error agregar brand");
+          });
+      }
+    },
+    closeDialogBrand() {
+      this.$v.$reset();
+      this.closeDialog();
     }
   }
 };
